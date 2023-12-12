b0f93bb50d7f3b0bf6358634288a9c77b46a0267
rsk94zoo <rachid.abdoulalime@gmail.com>
2023-12-12 23:33:33 +0100

    Changement de code, pour pouvoir bien afficher les noms des colonnes en haut du tableau et non pas à gauche du tableau (où les rows sont affichées)

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ from dash import Dash, html
 import pandas as pd
 
 # Lire le fichier CSV avec le délimiteur ';'
-df = pd.read_csv('/Users/kayanthan/PycharmProjects/Covid19_Python/data/data_covid19.csv', delimiter=';')
+df = pd.read_csv('data/data_covid19.csv', delimiter=';')
+
 
 # Créer une fonction pour générer le tableau HTML
 def generate_table(dataframe):
     table_rows = []
-    for col in dataframe.columns:
-        table_rows.append(
-            html.Tr([
-                html.Th(col),
-                *[html.Td(data) for data in dataframe[col].head(10)]
-                # Afficher les 10 premières valeurs de chaque colonne
-            ])
-        )
+
+    # Créer la première ligne avec les noms de colonne
+    header_row = [html.Th('Columns')] + [html.Th(col) for col in dataframe.columns]
+    table_rows.append(html.Tr(header_row))
+
+    # Créer les lignes avec les valeurs
+    for i in range(
+            min(10, len(dataframe))):  # Afficher les 10 premières lignes ou moins si la dataframe est plus petite
+        row_data = [html.Th('Row ' + str(i + 1))] + [html.Td(data) for data in dataframe.iloc[i]]
+        table_rows.append(html.Tr(row_data))
 
     return html.Table([
-        html.Thead(html.Tr([html.Th('Columns')] + [html.Th('Row ' + str(i + 1)) for i in range(10)])),
-        html.Tbody(table_rows)
+        html.Thead(table_rows[0]),  # Utiliser la première ligne comme en-tête
+        html.Tbody(table_rows[1:])  # Utiliser le reste comme corps
     ])
 
 
