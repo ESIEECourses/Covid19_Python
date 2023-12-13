d82d1475bea16ec6c8d4638727621a91b9128195
rsk94zoo <rachid.abdoulalime@gmail.com>
2023-12-13 22:59:59 +0100

    Comportement de classe carte.py à été transormée comme une vrai classe orienté objet pour obtenir un objet "Carte"

--- carte.py
+++ carte.py
@@ -4,58 +4,59 @@ import numpy as np
 from branca.colormap import LinearColormap
 from dash import Dash, html
 
-df = pd.read_csv('data/data_covid19.csv', delimiter=';')
+class Carte:
+    def __init__(self):
+        self.df = pd.read_csv('data/data_covid19.csv', delimiter=';')
+        self.df = self.df.dropna()
 
-# Supprime les lignes avec des valeurs manquantes
-df = df.dropna()
+        self.centre_lat, self.centre_lon = 46.6031, 1.8883
+        self.map_bounds = [[41, -5], [51, 9]]
 
-# Coordonnées du centre de la France
-centre_lat, centre_lon = 46.6031, 1.8883
+        self.min_decès = self.df['Total Décès'].min()
+        self.max_decès = self.df['Total Décès'].max()
 
-# Définit les limites de la carte pour la France et ses pays voisins
-map_bounds = [[41, -5], [51, 9]]  # [Latitude_min, Longitude_min], [Latitude_max, Longitude_max]
+        self.colormap = LinearColormap(colors=['orange', 'darkorange', 'red'],
+                                       index=[self.min_decès, (self.min_decès + self.max_decès) / 2, self.max_decès],
+                                       vmin=self.min_decès, vmax=self.max_decès)
 
-# Trouve la plage du nombre total de décès pour normaliser la taille des cercles
-min_decès = df['Total Décès'].min()
-max_decès = df['Total Décès'].max()
+        self.m = folium.Map(location=[self.centre_lat, self.centre_lon], zoom_start=6,
+                            max_bounds=True, min_zoom=6, max_zoom=10, bounds=self.map_bounds)
 
-# Définit une échelle de couleurs personnalisée
-colormap = LinearColormap(colors=['orange', 'darkorange', 'red'], index=[min_decès, (min_decès + max_decès) / 2, max_decès],
-vmin=min_decès, vmax=max_decès)
+        self.add_markers()
 
-# Crée une carte avec folium
-m = folium.Map(location=[centre_lat, centre_lon], zoom_start=6, max_bounds=True, min_zoom=6, max_zoom=10, bounds=map_bounds)
+    def add_markers(self):
+        for index, row in self.df.iterrows():
+            radius = np.log1p(row['Total Décès']) * 1.5
+            radius = max(radius, 3)
+            radius = min(radius, 10)
 
-# Ajoute des marqueurs pour chaque département
-for index, row in df.iterrows():
-    # Applique une échelle logarithmique à la taille des cercles
-    radius = np.log1p(row['Total Décès']) * 1.5
+            color = self.colormap(row['Total Décès'])
 
-    # Ajuste la plage des tailles de cercles pour éviter qu'ils ne deviennent trop grands lors du dézoom
-    radius = max(radius, 3)  # Taille minimale des cercles
-    radius = min(radius, 10)  # Taille maximale des cercles
+            folium.CircleMarker(
+                location=[float(coord) for coord in row['geo_point_2d'].split(', ')],
+                radius=radius,
+                color=color,
+                fill=True,
+                fill_color=color,
+                fill_opacity=0.7,
+                popup=row['Nom département'] + '<br>Total Décès: ' + str(row['Total Décès'])
+            ).add_to(self.m)
 
-    # Couleur de la colormap en fonction du nombre de décès
-    color = colormap(row['Total Décès'])
+    def get_map(self):
+        return self.m
 
-    folium.CircleMarker(
-        location=[row['geo_point_2d'].split(', ')[0], row['geo_point_2d'].split(', ')[1]],
-        radius=radius,
-        color=color,
-        fill=True,
-        fill_color=color,
-        fill_opacity=0.7,
-        popup=row['Nom département'] + '<br>Total Décès: ' + str(row['Total Décès'])
-    ).add_to(m)
 
 # Crée l'application Dash
 app = Dash(__name__)
+
+# Crée une instance de la classe Carte
+ma_carte = Carte()
 
 # Mise en page de l'application avec une largeur fixe de 40%
 app.layout = html.Div([
     html.H4(children='Carte des décès par département'),
     # dcc.Graph pour afficher la carte
-    html.Iframe(srcDoc=m.get_root().render(), width='40%', height='600px')
+    html.Iframe(srcDoc=ma_carte.get_map().get_root().render(), width='40%', height='600px')
 ])
 
 if __name__ == '__main__':
