77220193517cb27fd57c0042d3f018074da775e5
rsk94zoo <rachid.abdoulalime@gmail.com>
2023-12-15 14:01:43 +0100

    Opti code encore main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from dash import Dash, html
 from navigator_dashboard import NavigatorDashBoard
 
-def main():
-    navigator_dashboard = NavigatorDashBoard()
-    app = navigator_dashboard.createDashApplication()
+class Main:
+    def main():
+        navigator_dashboard = NavigatorDashBoard()
+        app = navigator_dashboard.createDashApplication()
 
-    app.run_server(debug=True, use_reloader=False)
+        app.run_server(debug=True, use_reloader=False)
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
+
