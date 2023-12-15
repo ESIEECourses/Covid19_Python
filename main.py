f5cc0d2c82525e22d923472645c38a85a9978494
rsk94zoo <rachid.abdoulalime@gmail.com>
2023-12-15 13:59:41 +0100

    Optimisation de code pour les classes main.py et navigator_dashboard + ajout d'affichage d'info zone quand curseur passe dessus

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from dash import Dash, html
-from carte import Carte
 from navigator_dashboard import NavigatorDashBoard
 
 def main():
-    carte = Carte()
-    navigator_dashboard = NavigatorDashBoard(carte)
+    navigator_dashboard = NavigatorDashBoard()
+    app = navigator_dashboard.createDashApplication()
 
-    navigator_dashboard.createDashApplication()
-    navigator_dashboard.app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True, use_reloader=False)
 
 if __name__ == '__main__':
     main()
--- navigator_dashboard.py
+++ navigator_dashboard.py
@@ -1,17 +1,20 @@
 from dash import Dash, html
+from carte import Carte
 
 class NavigatorDashBoard:
 
-    def __init__(self, carte):
-        self.carte = carte
+    def __init__(self):
+        self.carte = Carte()
 
     def createDashApplication(self):
         self.app = Dash(__name__)
 
         self.app.layout = html.Div([
-            html.H4(children='Carte des décès par département'),
+            html.H4(children='DashBoard COVID-19'),
             # dcc.Graph to display the Carte
             html.Iframe(srcDoc=self.carte.get_map().get_root().render(), width='40%', height='600px')
         ])
+
+        return self.app
 
 
