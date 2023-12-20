e88dac906cc6c6d067ff9d86ce7832238fc2745b
kayanthan <pro.nkayan@gmail.com>
2023-12-20 15:50:47 +0100

    update layout

--- Graphe.py
+++ Graphe.py
@@ -1,28 +1,29 @@
 import pandas as pd
 import plotly.express as px
 
-data = pd.read_csv('data/Covid.csv', delimiter=';')
+class Graphe:
+    def __init__(self):
+        self.data = pd.read_csv('data/Covid.csv', delimiter=';')
+        self.data = self.data.dropna()
 
-# Filtrer les données où l'âge n'est pas égal à zéro
-filtered_data = data[data['Valeur de la variable'] != 0]
+        self.filtered_data = self.data[self.data['Valeur de la variable'] != 0]
 
-# Regrouper les données par tranche de 10 pour l'âge
-grouped_data = filtered_data.groupby((filtered_data['Valeur de la variable'] // 10) * 10)['Nombre cumulé de doses n°1'].sum().reset_index()
+        self.grouped_data = self.filtered_data.groupby((self.filtered_data['Valeur de la variable'] // 10) * 10)['Nombre cumulé de doses n°1'].sum().reset_index()
 
-# Calculer le pourcentage par rapport au total
-total_doses = grouped_data['Nombre cumulé de doses n°1'].sum()
-grouped_data['Pourcentage'] = (grouped_data['Nombre cumulé de doses n°1'] / total_doses) * 100
+        self.total_doses = self.grouped_data['Nombre cumulé de doses n°1'].sum()
+        self.grouped_data['Pourcentage'] = (self.grouped_data['Nombre cumulé de doses n°1'] / self.total_doses) * 100
 
-# Intervalles pour chaque tranche
-grouped_data['Intervalles'] = grouped_data['Valeur de la variable'].astype(str) + '-' + (grouped_data['Valeur de la variable'] + 9).astype(str)
-# Arrondir le pourcentage
-grouped_data['Pourcentage'] = grouped_data['Pourcentage'].round(2)
+        # Intervalles pour chaque tranche
+        self.grouped_data['Intervalles'] = self.grouped_data['Valeur de la variable'].astype(str) + '-' + (self.grouped_data['Valeur de la variable'] + 9).astype(str)
+        # Arrondir le pourcentage
+        self.grouped_data['Pourcentage'] = self.grouped_data['Pourcentage'].round(2)
 
-fig = px.bar(grouped_data, x='Intervalles', y='Pourcentage',
-             labels={"Pourcentage": "Pourcentage de doses n°1 ", "Intervalles": "Tranches d'âges "},
-             title="Pourcentage de doses n°1 par tranche d'âges")
+        self.fig = px.bar(self.grouped_data, x='Intervalles', y='Pourcentage',
+                     labels={"Pourcentage": "Pourcentage de doses n°1 ", "Intervalles": "Tranches d'âges "},
+                     title="Pourcentage de doses n°1 par tranche d'âges")
 
-# Affiche les pourcentages sur les barres
-fig.update_traces(texttemplate='%{y:f}%', textposition='outside')
+        self.fig.update_traces(texttemplate='%{y:f}%', textposition='outside')
 
-fig.show()
+
+    def get_dash(self):
+        return self.fig
--- navigator_dashboard.py
+++ navigator_dashboard.py
@@ -1,10 +1,12 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 from carte import Carte
+from Graphe import Graphe
 
 class NavigatorDashBoard:
 
     def __init__(self):
         self.carte = Carte()
+        self.graphe = Graphe()
 
     def createDashApplication(self):
         self.app = Dash(__name__)
@@ -14,11 +16,14 @@ class NavigatorDashBoard:
             children=[
                 html.H2(children='DashBoard Covid-19', style={'textAlign': 'center', 'color': 'white'}),
                 html.Br(),
-                html.H4(children='Représentation géographique du total de décès par départementen France', style={'textAlign': 'center', 'color': 'white'}),
-                # dcc.Graph to display the Carte
-                html.Iframe(srcDoc=self.carte.get_map().get_root().render(), width='40%', height='600px')
+                html.H4(children='Représentation géographique du total de décès par département en France', style={'font-size': '24px', 'textAlign': 'center', 'color': 'white'}),
+                html.Iframe(srcDoc=self.carte.get_map().get_root().render(), width='40%', height='600px' ),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                dcc.Graph(figure=self.graphe.get_dash())
             ],
-            style={'height': '100vh'},  # Darker Blue color
+            style={'height': '100vh', 'backgroundColor': '#0d0d26'},
         )
 
         # Charge le fichier CSS
